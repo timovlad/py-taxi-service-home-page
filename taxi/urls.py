7133c1e5d16bfbd95cbd3c13c2ed22885f80e8a5
Vlad Tymoshko <slradbez@gmail.com>
2024-12-28 17:33:49 +0100

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'taxi'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Путь для домашней страницы
+    path('', views.index, name='index'),  # start page
 ]
