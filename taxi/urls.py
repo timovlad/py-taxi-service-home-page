b317166d965f601069e80e1cb020b68c78505f76
Vlad Tymoshko <slradbez@gmail.com>
2024-12-29 09:37:27 +0100

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'taxi'
+app_name = "taxi"
 
 urlpatterns = [
-    path('', views.index, name='index'),  # start page
+    path("", views.index, name="index"),  # start page
 ]
